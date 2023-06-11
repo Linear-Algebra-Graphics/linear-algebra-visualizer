ced84bc82c4354cd4d69c9b1e60edeb96d7587aa
edison.hao112@gmail.com <edison.hao112@gmail.com>
2023-06-11 14:06:59 -0700

    dont remember what I did

--- graph.js
+++ graph.js
@@ -1,17 +1,65 @@
 
-
-//only x y axis rn
+//x, y, z axis
 class Graph {
     
     constructor(canvas) {
         this.canvas = canvas
+        this.height = canvas.height
+        this.width = canvas.width
         this.ctx = canvas.getContext("2d")
         this.baseX = this.canvas.width/2
         this.baseY = this.canvas.height/2
         this.drawnVectors = [];
-        drawBasis(this.ctx, this.canvas.width, this.canvas.height)
+        this.axisVectors = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+        //this.axisVectors = [[[0, this.height/2],[this.width, this.height/2]],[[this.width/2, 0],[this.width/2, this.height]]]; //stores endpoints
+        this.drawAxis(this.ctx, this.axisVectors[0], this.axisVectors[1])
     }
 
+    drawAxis() {
+        this.ctx.strokeStyle = "blue"
+        this.ctx.lineWidth = 2
+        let axisLength = this.baseX
+        //              calculates x y needed to keep vector length at axisLength
+        // let xScale = Math.sqrt((axisLength*axisLength)/
+        //                         parseFloat(this.axisVectors[0][0] * this.axisVectors[0][0]
+        //                          + this.axisVectors[0][1] * this.axisVectors[0][1]))
+        // let yScale = Math.sqrt((axisLength*axisLength)/
+        //                         parseFloat(this.axisVectors[1][0] * this.axisVectors[1][0]
+        //                         + this.axisVectors[1][1] * this.axisVectors[1][1]))
+        let xScale = axisLength;
+        let yScale = axisLength;
+        let xAxis = [(xScale*this.axisVectors[0][0]), (xScale*this.axisVectors[0][1])]
+        let yAxis = [(yScale*this.axisVectors[1][0]), (yScale*this.axisVectors[1][1])]
+
+        //x axis right
+        this.ctx.beginPath()
+        this.ctx.moveTo(this.baseX, this.baseY)
+        this.ctx.lineTo(this.baseX + xAxis[0], this.baseY - xAxis[1])
+        this.ctx.stroke()
+        //x axis left
+        this.ctx.beginPath()
+        this.ctx.moveTo(this.baseX, this.baseY)
+        this.ctx.lineTo(this.baseX - xAxis[0], this.baseY + xAxis[1])
+        this.ctx.stroke()
+        
+        this.ctx.strokeStyle = "red"
+        //y axis top
+        this.ctx.beginPath()
+        this.ctx.moveTo(this.baseX, this.baseY)
+        this.ctx.lineTo(this.baseX + yAxis[0], this.baseY - yAxis[1])
+        this.ctx.stroke()
+        //y axis bottom
+        this.ctx.beginPath()
+        this.ctx.moveTo(this.baseX, this.baseY)
+        this.ctx.lineTo(this.baseX - yAxis[0], this.baseY + yAxis[1])
+        this.ctx.stroke()
+        
+        //write axis labels
+        this.ctx.font = "30px Arial"
+        this.ctx.fillStyle = "black"
+        this.ctx.fillText("x", this.baseX - xAxis[0] + 10, this.baseY + xAxis[1] - 10)
+        this.ctx.fillText("y", this.baseX + yAxis[0] - 20, this.baseY - yAxis[1] + 20)
+    }
 
     /**
      * takes a 2d vector [x, y] and draws it on the canvas
@@ -24,6 +72,7 @@ class Graph {
             this.ctx.moveTo(this.baseX, this.baseY)
             this.ctx.lineTo(vector[0] + this.baseX, this.baseY - vector[1])
         this.ctx.stroke()
+        this.drawnVectors.push(vector)
     }
 
     /** */
@@ -36,25 +85,3 @@ class Graph {
 
     }
 }
-
-
-// draws the initial x,y basis of the graph
-// @param {*} ctx : 2d context of canvas
-// @param {*} width : width of canvas
-// @param {*} height : height of canvas
-// @requires : x and y to be nonegative
-function drawBasis(ctx, width, height) {
-    ctx.strokeStyle = "black"
-    //x axis
-    ctx.beginPath()
-        ctx.moveTo(0, height/2)
-        ctx.lineTo(width, height/2)
-    ctx.stroke()
-    //y axis
-    ctx.beginPath()
-        ctx.moveTo(width/2, 0)
-        ctx.lineTo(width/2, height)
-    ctx.stroke()
-}
-
-
--- script.js
+++ script.js
@@ -5,15 +5,23 @@ let yInput = document.getElementById("y")
 let graphButton = document.getElementById("graphButton")
 let vectorColors = document.getElementById("vector colors")
 
-canvas.width = 600
-canvas.height = 600
+displayWidth = 600
+displayHeight = 600
+canvas.style.width = displayWidth + "px"
+canvas.style.height = displayHeight + "px"
+canvas.width = displayWidth * 2
+canvas.height = displayHeight *2
 
 let test_graph = new Graph(canvas);
 
+
+//scale needs to change
+//since with scale = 1 graphed vectors are too small
 graphButton.addEventListener("click", function() {
-    console.log("hooray hooray javascript!")
-    let x = parseInt(xInput.value)
-    let y = parseInt(yInput.value)
+    //console.log("hooray hooray javascript!")
+    let scale = 1
+    let x = parseInt(xInput.value) * scale
+    let y = parseInt(yInput.value) * scale
     let color = vectorColors.value
     test_graph.drawVector([x, y], color)
 })
