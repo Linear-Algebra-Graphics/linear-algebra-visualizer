9fda6e11e0b4cc88638214e4f019ccf3573c4824
edison.hao112@gmail.com <edison.hao112@gmail.com>
2023-06-11 18:22:42 -0700

    Axis appears, and can add vectors to graph.

--- graph.js
+++ graph.js
@@ -1,7 +1,24 @@
+//import { matrixVectorMultiplication } from "./matrixMult"
 
-//x, y, z axis
+//Notation
+//vector: [a, b, c] is a column vector
+//
+//        |a|
+//        |b|
+//        |c|
+//
+//matrix: [[a,b,c],[d,e,f],[g,h,i]] is the matrix
+//
+//        |a d g|
+//        |b e h|
+//        |c f i|
+//
 class Graph {
     
+    /**
+     * creates an instance of an R3 graph
+     * @param {*} canvas canvas on which the graph is drawn 
+     */
     constructor(canvas) {
         this.canvas = canvas
         this.height = canvas.height
@@ -11,61 +28,21 @@ class Graph {
         this.graphCenterX = this.canvas.width/2
         this.graphCenterY = this.canvas.height/2
         
-        this.drawnVectors = [];
-        
-        let xAxis = {cords : {x: 10, y: 0, z: 0}, color: "blue", arrow: false}
-        let yAxis = {cords : {x: 0, y: 10, z: 0}, color: "red", arrow: false}
-        let zAxis = {cords : {x: 0, y: 0, z: 10}, color: "green", arrow: false}
-        this.axisVectors = [xAxis, yAxis, zAxis]
+        this.drawnObjects = [];
 
         // This is the number of graph units from edge to edge. E.g if the canvas is 600x600px we want
         // the length of 20 units to be 600 px.
         this.numOfGraphUnitsEdgeToEdge = 20;
-
-        this.backgroundColor = "white";
-
-        this.showAxis     = true
-        this.lables       = false
-        this.fullAxis     = true
-        this.infinateAxis = false
-        this.zeroZeroDot  = true
-
+        this.backgroundColor           = "white"
+        // I matrix is default
+        this.basis                     = [[1,0,0],[0,1,0],[0,0,1]]
+        this.showAxis                  = true
+        this.graphAxis                 = new Axis(this)
+        this.graphGrid                 = new Grid(this)
     }
 
-    addAxisVectors() {
-        //              calculates x y needed to keep vector length at axisLength
-        // let xScale = Math.sqrt((axisLength*axisLength)/
-        //                         parseFloat(this.axisVectors[0][0] * this.axisVectors[0][0]
-        //                          + this.axisVectors[0][1] * this.axisVectors[0][1]))
-        // let yScale = Math.sqrt((axisLength*axisLength)/
-        //                         parseFloat(this.axisVectors[1][0] * this.axisVectors[1][0]
-        //                         + this.axisVectors[1][1] * this.axisVectors[1][1]))
-        
-        // let xScale = axisLength;
-        // let yScale = axisLength;
-        // let xAxis = [(xScale*this.axisVectors[0][0]), (xScale*this.axisVectors[0][1])]
-        // let yAxis = [(yScale*this.axisVectors[1][0]), (yScale*this.axisVectors[1][1])]
-
-
-        // Draw x axis
-        this.addVector(this.axisVectors[0]);
-        // Draw y axis
-        this.addVector(this.axisVectors[1]);
-        
-
-
-        
-        // if (lables == true) {
-        //     //write axis labels
-        //     this.ctx.font = "30px Arial"
-        //     this.ctx.fillStyle = "black"
-        //     this.ctx.fillText("x", this.graphCenterX - xAxis[0] + 10, this.graphCenterY + xAxis[1] - 10)
-        //     this.ctx.fillText("y", this.graphCenterX + yAxis[0] - 20, this.graphCenterY - yAxis[1] + 20)
-        // }
-    }
-
-    addVector(vector) {
-        this.drawnVectors.push(vector)
+    addObject(object) {
+        this.drawnObjects.push(object)
     }
 
     /** */
@@ -82,25 +59,180 @@ class Graph {
         // Clear screen / add background
         this.ctx.fillStyle = this.backgroundColor
         this.ctx.fillRect(0,0, this.canvas.width, this.canvas.height);
-
+        
         if (this.showAxis == true) {
-            this.addAxisVectors();
+            this.graphAxis.draw()
         }
 
-        // Draw all vectors
-        for(let i=0; i<this.drawnVectors.length; i++) {
-
-            let currentVector = this.drawnVectors[i];
-
-            this.ctx.strokeStyle = currentVector.color
-
-            this.ctx.beginPath()
-                this.ctx.moveTo(this.graphCenterX, this.graphCenterY)
-                this.ctx.lineTo(currentVector.cords.x + this.graphCenterX, this.graphCenterY - currentVector.cords.y)
-            this.ctx.stroke()
+        if (this.showGrid == true) {
+            this.graphGrid.draw()
         }
+        
+        // Draw all objects
 
+        for(let i = 0; i < this.drawnObjects.length; i++) {
+            this.drawnObjects[i].draw();
+        }
     }
 
 
+}
+
+//me
+class Axis {
+    
+    /**
+     * creates an instance of an Axis
+     * @param {*} graph the graph on which the axis are drawn
+     */
+    constructor(graph) {
+        this.graph = graph
+        
+        this._xAxis = new Vector(this.graph, [10,0,0], "blue", false)
+        this._yAxis = new Vector(this.graph, [0,10,0], "red", false)
+        this._zAxis = new Vector(this.graph, [0,0,10], "green", false)
+        
+        this._xAxisNeg = new Vector(this.graph, [-10,0,0], "blue", false)
+        this._yAxisNeg = new Vector(this.graph, [0,-10,0], "red", false)
+        this._zAxisNeg = new Vector(this.graph, [0,0,-10], "green", false)
+    
+        // Default
+        this.fullAxis     = true
+        this.infiniteAxis = false
+        this.zeroZeroDot  = true
+    }
+    
+    /**
+     * draws x, y, z axis
+     */
+    draw() {
+        if (this.zeroZeroDot) {
+            this.graph.ctx.fillRect(this.graph.centerX, this.graph.centerY, 1, 1);
+        }
+
+        if(this.infiniteAxis == false) {
+            //console.log("trying to draw successful")
+            if (this.fullAxis) {
+                this._xAxisNeg.draw()
+                this._yAxisNeg.draw()
+                this._zAxisNeg.draw()
+            }
+            this._xAxis.draw()
+            this._yAxis.draw()
+            this._zAxis.draw()
+
+       } else {
+            //remember to set back matrixMultiplcation matix if changed to finite axis
+       }
+    }
+    
+    
+    setAxis(size, arrow) {
+
+        // Update the cords
+        this._xAxis.cords = [size, 0, 0]
+        this._xAxis.cords = [0, size, 0]
+        this._xAxis.cords = [0, 0, size]
+
+        this._xAxisNeg.cords = [-1*size, 0, 0]
+        this._yAxisNeg.cords = [0, -1*size, 0]
+        this._zAxisNeg.cords = [0, 0, -1*size]
+
+        
+        // Update the arrows
+        this._xAxis.arrow = arrow
+        this._xAxis.arrow = arrow
+        this._xAxis.arrow = arrow
+
+        this._xAxisNeg.arrow = arrow
+        this._yAxisNeg.arrow = arrow
+        this._zAxisNeg.arrow = arrow   
+    }
+}
+
+
+
+class Vector {
+
+    /**
+     * creates an instance of a vector
+     * @param {*} graph graph on which vector is drawn
+     * @param {*} cords coordinates (x, y, z) of vector
+     * @param {*} color color of vector
+     * @param {*} arrow true if vector has arrow at the end
+     */
+    constructor(graph, cords, color, arrow){
+        this.graph         = graph
+        this.cords         = cords
+        this.color         = color
+        this.arrow         = arrow
+    }
+
+    /**
+     * draws the vector on the graph
+     */
+    draw() {
+        this.graph.ctx.strokeStyle = this.color
+        
+        const scale = this.graph.canvas.width / this.graph.numOfGraphUnitsEdgeToEdge
+        
+        let centerX = this.graph.graphCenterX
+        let centerY = this.graph.graphCenterY
+        
+        // First apply the basis, then the applied matrix. Not sure if this is the correct order for all situations...
+        let updatedCords = matrixVectorMultiplication(this.graph.basis, this.cords)
+        //console.log(updatedCords)
+
+        let finalX  = centerX + scale * updatedCords[0]
+        // Flipped because Y axis is zero at the top
+        let finalY  = centerY - scale * updatedCords[1]
+        // console.log("finalX: " + finalX)
+        // console.log("finalY: " + finalY)
+        this.graph.ctx.beginPath()
+            this.graph.ctx.moveTo(centerX, centerY)
+            this.graph.ctx.lineTo(finalX, finalY)
+        this.graph.ctx.stroke()
+
+        if(this.arrow == true) {
+            // Draw the arrow
+        }
+    }
+
+
+}
+
+class Grid {
+    constructor(){
+        
+    }
+
+    draw() {
+        
+    }
+}
+
+
+
+/**
+ * does Ax = b multiplication 
+ * @param {*} matrix matrix A
+ * @param {*} vector vector x
+ * @returns vector Ax = b
+ */
+function matrixVectorMultiplication (matrix, vector) {
+    //console.log(matrix)
+    //console.log("vector " + vector)
+    let returnVector= new Array(vector.length)
+    //console.log("help1")
+    for (let i=0; i<returnVector.length; i++) {
+        returnVector[i] = 0
+    }
+    //console.log("help2")
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            returnVector[i] = returnVector[i] + matrix[i][j]*vector[j];
+        }
+    }
+    //console.log(returnVector)
+    return returnVector
 }
--- script.js
+++ script.js
@@ -5,25 +5,29 @@ let yInput = document.getElementById("y")
 let graphButton = document.getElementById("graphButton")
 let vectorColors = document.getElementById("vector colors")
 
-displayWidth = 600
+displayWidth  = 600
 displayHeight = 600
+
 canvas.style.width = displayWidth + "px"
 canvas.style.height = displayHeight + "px"
 canvas.width = displayWidth * 2
-canvas.height = displayHeight *2
+canvas.height = displayHeight * 2
+
+// canvas.width = displayWidth 
+// canvas.height = displayHeight
 
 let test_graph = new Graph(canvas);
 
-
-//scale needs to change
-//since with scale = 1 graphed vectors are too small
-graphButton.addEventListener("click", function() {
-    //console.log("hooray hooray javascript!")
-    let scale = 1
-    let x = parseInt(xInput.value) * scale
-    let y = parseInt(yInput.value) * scale
-    let color = vectorColors.value
-    test_graph.drawVector([x, y], color)
-})
-
 test_graph.draw();
+
+
+
+// //scale needs to change
+// //since with scale = 1 graphed vectors are too small
+graphButton.addEventListener("click", function() {
+    let x = parseInt(xInput.value)
+    let y = parseInt(yInput.value)
+    let color = vectorColors.value
+    test_graph.addObject(new Vector(test_graph, [x, y, 0], color, false))
+    test_graph.draw()
+})
